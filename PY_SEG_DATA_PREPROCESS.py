ed3ae3d3bcbf62f5d534b039fa2c46dd3bbe1b9c
Taou <756058918@qq.com>
2021-01-05 21:16:16 +0800

    Preprocess data

---
+++ PY_SEG_DATA_PREPROCESS.py
@@ -0,0 +1,100 @@
+import re
+import numpy as np
+import pinyin
+
+
+'''
+This is used for preprocessing data from icwb2-data.
+Non-Chinese symbols are intepreted as delimiter.
+'''
+
+DATA_DIR = './icwb2-data/training/pku_training.utf8'                # Path of data we will use
+
+def LoadData(dir):
+    '''
+    This is used for loading the raw training data whose
+    is encoded by UTF-8.
+    '''
+    f = open(dir, encoding='utf-8')   
+    data = f.readlines()  
+    f.close()             
+
+    return data
+
+
+def EliminateNonChinese(SentenceList):
+    '''
+    Without taking non-Chinese symbols into consideration,
+    we may divide each sentence into many subsentence by 
+    the position of some special characters.
+
+    Note that the single word will be deleted, leading to
+    nearly no effect on the performance.
+    '''
+    pattern1 = re.compile(r'[^\u4e00-\u9fa5\s]+')                   # Pattern that keeping only Chinese and space
+    FinalSentenceList = []
+    for i in range(len(SentenceList)):
+        SentenceList[i] = re.sub('\n','', SentenceList[i])          # Delete \n
+        OnlyChinese = re.sub(pattern1, '\n', SentenceList[i])       # Keep only Chinese and space
+
+        # Find subsentences
+        SubSentence = ''
+        for s in OnlyChinese:                                       
+            if s != '\n':
+                SubSentence = SubSentence + s
+            else:
+                pattern2 = re.compile(u'[\u4e00-\u9fa5]')           # Pattern that checking the existence of Chinese
+                ValidSentence = pattern2.search(SubSentence)        # Check if there is Chinese in the sentence
+                if ValidSentence:                                   # If so, add it as a new sentence
+                    SubSentence = SubSentence.strip()
+                    SplitedSubSentence = SubSentence.split('  ')
+
+                    # Omit the single-word sentence
+                    if len(SplitedSubSentence) == 1 and \
+                        len(SplitedSubSentence[0]) == 1:    
+                        SubSentence = ''                            
+                        continue
+
+                    FinalSentenceList.append(SplitedSubSentence)               
+                    SubSentence = ''
+                
+    return FinalSentenceList
+
+
+def ConvertingAndTagging(FinalSentenceList):
+    '''
+    Convert each Chinese symbols to Pinyin and use 
+    4-ary tagging [S, B, M, E] -> {0, 1, 2, 3}
+    '''
+    S, B, M, E = 0, 1, 2, 3
+    PYList = []
+    TagList = []
+    for s in FinalSentenceList:
+        SubPYList = []
+        SubTagList = []
+        for w in s:
+            if len(w) == 1:
+                SubPYList.append(pinyin.get(w, format='strip', delimiter=" "))     # Convert to pinyin
+                SubTagList.append(S)
+
+            # Tagging when more than one symbols
+            elif len(w) >= 1:
+                for i in range(len(w)):
+                    SubPYList.append(pinyin.get(w[i], format='strip', delimiter=" "))
+                    if i == 0:
+                        SubTagList.append(B)
+                    elif i == len(w) - 1:
+                        SubTagList.append(E)
+                    else:
+                        SubTagList.append(M)
+        
+        PYList.append(SubPYList)
+        TagList.append(SubTagList)
+
+    return PYList, TagList
+
+# Save data
+if __name__ == "__main__":
+    SentenceList = LoadData(DATA_DIR)
+    FinalSentenceList = EliminateNonChinese(SentenceList)
+    PYList, TagList = ConvertingAndTagging(FinalSentenceList)
