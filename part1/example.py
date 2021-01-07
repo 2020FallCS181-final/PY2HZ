5fb583c2adeb0ce584d2c8309eb3fe5578f3082b
zhengyt1 <zhengyt1@shanghaitech.edu.cn>
2021-01-07 13:01:04 +0800

    add test acc part

--- part1/example.py
+++ part1/example.py
@@ -4,4 +4,12 @@ tree = Trie_Tree('root')
 print('***************begin to search********************')
 print(tree.search('woaibeijingtiananmen'))
 print(tree.search('maixiangxinshiji'))
-
+with open ('test_pinyin_content.utf8', 'r') as f:
+    pinyin_content = f.read().split()
+step = 10
+t = total = 0
+for i in range(0, len(pinyin_content), step):
+    if tree.search(''.join(pinyin_content[i:i+step])) == ' '.join(pinyin_content[i:i+step]):
+        t += 1
+    total += 1
+print('accuracy when input {} consecutive pinyin is {}'.format(step, t / total))
