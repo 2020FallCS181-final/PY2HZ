054e7bb8ab5af5fcd07653f4fabd068d52f61912
zhengyt1 <zhengyt1@shanghaitech.edu.cn>
2021-01-07 20:17:24 +0800

    fix a small wrong

--- part1/example.py
+++ part1/example.py
@@ -3,4 +3,4 @@ from seg_with_trie_consider_next import Trie_Tree
 tree = Trie_Tree('root')
 print('***************begin to search********************')
 print(tree.search('woaibeijingtiananmen'))
-print(tree.search('maixiangxinshiji'))
+print(tree.search('zailiangdeshirentoutengdedengguangxia'))
