1ea9130144821717ded00d478f80f7e20615e278
WuDaqian <wudq1@shanghaitech.edu.cn>
2021-01-08 10:12:04 +0800

    1+3

---
+++ part3/seg_with_trie_consider_next.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+class Trie_Tree(object):
+    class Trie_Tree_Node(object):
+        def __init__(self, key):
+            self.key = key
+            self.isTerminal = False
+            self.appearance = 0
+            self.children = {}
+    
+    def __init__(self, root):
+        self.root = self.Trie_Tree_Node(root)
+        with open ('pinyin_to_init_trie_tree.utf8', 'r') as f:
+            pinyin_content = f.read().split()
+        for item in pinyin_content:
+            self.insert(item)
+
+    def insert(self, word):
+        parent_node = self.root
+        for key in word:
+            if key not in parent_node.children.keys():
+                parent_node.children[key] = self.Trie_Tree_Node(key)
+            parent_node = parent_node.children[key]
+        parent_node.isTerminal = True
+        parent_node.appearance += 1
+
+    def search(self, word):
+        fuyin = 'bcdfghjklmnpqrstwxyz'
+        word += ' '
+        spell = ''
+        while len(word) > 1:
+            # import pdb; pdb.set_trace()
+            segments = []
+            appearances = []
+            parent_node = self.root
+            segment = ''
+            choose_seg = ''
+            for i in range(len(word)):
+                key = word[i]
+                if key not in parent_node.children.keys():
+                    if (len(segments) == 0):
+                        return spell + 'cuowu'
+                    if len(segments) == 1:
+                        choose_seg = segments[-1]
+                    else :
+                        for i in range(len(segments)):
+                            choose_seg = segments[i]
+                            pre_seg = self.pre_search(word[len(choose_seg):len(choose_seg)+5])
+                            if pre_seg == False:
+                                appearances[i] = 0
+                        for i in range(len(segments) - 1, -1, -1):
+                            if appearances[i] != 0: 
+                                if i > 0 and word[len(segments[i])] in 'aeiouv' \
+                                         and word[len(segments[i])-1] not in 'aeiouv' \
+                                         and word[len(segments[i])-3:len(segments[i])] != 'ian' \
+                                         and word[len(segments[i]):len(segments[i])+2] != 'er' \
+                                         and word[len(segments[i])-3:len(segments[i])] != 'uan' \
+                                         and word[:len(segments[i])+2] != 'qinai' \
+                                         and word[len(segments[i]):len(segments[i])+5] != 'emeng':
+                                    continue
+                                else:
+                                    choose_seg = segments[i]
+                                break
+                    spell += choose_seg
+                    spell += " "
+                    word = word[len(choose_seg):]
+                    break
+                segment += key
+                parent_node = parent_node.children[key]
+                if parent_node.isTerminal:
+                    segments.append(segment)
+                    appearances.append(parent_node.appearance)
+        return spell[:-1]
+    
+    def pre_search(self, word):
+        fuyin = 'bcdfghjklmnpqrstwxyz'
+        spell = ''
+        if word[-1] != ' ':
+            word += ' '
+        while len(word) > 1:
+            segments = []
+            appearances = []
+            parent_node = self.root
+            segment = ''
+            for i in range(len(word)):
+                key = word[i]
+                if key not in parent_node.children.keys():
+                    if (len(segments) == 0):
+                        return False
+                    return True
+                segment += key
+                parent_node = parent_node.children[key]
+                if parent_node.isTerminal:
+                    segments.append(segment)
+                    appearances.append(parent_node.appearance)
+        return True
--- part3/test.py
+++ part3/test.py
@@ -5,6 +5,9 @@ import utils
 from ChineseTone import PinyinHelper
 from tqdm import tqdm
 
+from seg_with_trie_consider_next import Trie_Tree
+tree = Trie_Tree('root')
+
 def norm_pinyin(pylist):
 	result = []
 	for pinyin in pylist:
@@ -17,27 +20,33 @@ def norm_pinyin(pylist):
 	return result
 
 with open('./test3/test_set_9plus.json') as f:
-	test_set = json.load(f)
+	test_set_9plus = json.load(f)
 
+with open('./test3/test_set_3to5.json') as f:
+	test_set_3to5 = json.load(f)
+
+with open('./test3/test_set_6to8.json') as f:
+	test_set_6to8 = json.load(f)
 hmm = HmmParam()
 count_single = 0
 correct_single = 0
 count_sentence = 0
 correct_sentence = 0
 
-for i in tqdm(range(len(test_set))):
+for i in tqdm(range(len(test_set_3to5))):
 	count_sentence += 1
-	test = test_set[i]
+	test = test_set_3to5[i]
 	flag = True
+	test_py = tree.search(''.join(test['py'])).split(' ')
+	# print(test_py)
 	try:
-		answer = Viterbi(hmm,test['py'],5)[0].path
+		answer = Viterbi(hmm,test_py,5)[0].path
 	except:
 		try:
-			answer = Viterbi(hmm,norm_pinyin(test['py']),5)[0].path
+			answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
 		except:
 			continue
-
-	ground_truth = test['hz']
+	ground_truth = [c for c in ''.join(test['hz'])]
 	for idx,an in enumerate(answer):
 		count_single += 1
 		if an == ground_truth[idx]:
@@ -46,6 +55,66 @@ for i in tqdm(range(len(test_set))):
 			flag = False
 	if flag:
 		correct_sentence += 1
+print('3-5')
+print('single:',correct_single/count_single)
+print('sentence:',correct_sentence/count_sentence)
 
+count_single = 0
+correct_single = 0
+count_sentence = 0
+correct_sentence = 0
+for i in tqdm(range(len(test_set_6to8))):
+	count_sentence += 1
+	test = test_set_6to8[i]
+	flag = True
+	test_py = tree.search(''.join(test['py'])).split(' ')
+	# print(test_py)
+	try:
+		answer = Viterbi(hmm,test_py,5)[0].path
+	except:
+		try:
+			answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
+		except:
+			continue
+	ground_truth = [c for c in ''.join(test['hz'])]
+	for idx,an in enumerate(answer):
+		count_single += 1
+		if an == ground_truth[idx]:
+			correct_single += 1
+		else:
+			flag = False
+	if flag:
+		correct_sentence += 1
+print('6-8')
+print('single:',correct_single/count_single)
+print('sentence:',correct_sentence/count_sentence)
+
+count_single = 0
+correct_single = 0
+count_sentence = 0
+correct_sentence = 0
+for i in tqdm(range(len(test_set_9plus))):
+	count_sentence += 1
+	test = test_set_9plus[i]
+	flag = True
+	test_py = tree.search(''.join(test['py'])).split(' ')
+	# print(test_py)
+	try:
+		answer = Viterbi(hmm,test_py,5)[0].path
+	except:
+		try:
+			answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
+		except:
+			continue
+	ground_truth = [c for c in ''.join(test['hz'])]
+	for idx,an in enumerate(answer):
+		count_single += 1
+		if an == ground_truth[idx]:
+			correct_single += 1
+		else:
+			flag = False
+	if flag:
+		correct_sentence += 1
+print('9+')
 print('single:',correct_single/count_single)
 print('sentence:',correct_sentence/count_sentence)
