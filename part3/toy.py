ea7b2ade5b4bb8f4ab9534af6c641ed3a4b56094
WuDaqian <wudq1@shanghaitech.edu.cn>
2021-01-08 20:17:51 +0800

    experiment

--- part3/toy.py
+++ part3/toy.py
@@ -2,12 +2,35 @@ import utils
 from viterbi import Viterbi
 
 from utils import HmmParam
+def norm_pinyin(pylist):
+	result = []
+	for pinyin in pylist:
+	    if 'ue' in pinyin:
+	        result.append(pinyin.replace('ue','ve'))
+	    elif 'ng' == pinyin:
+	        result.append('en')
+	    else:
+	    	result.append(pinyin)
+	return result
 
 hmm = HmmParam()
-print(hmm.emission_table['data']['学校'])
+
+from seg_with_trie_consider_next import Trie_Tree
+# tree = Trie_Tree('root')
+
 # print(hmm.emission_table['data']['长'])
-# print(hmm.transition_table['data']['爱']['北京'])
-# print(hmm.transition_table['data']['爱'])
-print(hmm.py2hz_dict['ve'])
+print(hmm.transition_table['data']['智能'])
+# print(hmm.transition_table['data']['爱']['不'])
+# print(hmm.transition_table['data']['爱']['着'])
+# print(hmm.transition_table['data']['爱']['饶舌'])
+# print(hmm.transition_table['data']['捱'])
+# print(hmm.py2hz_dict['ve'])
+
+# test_py = ['wo','ai','beijing','tiananmen']
+# try:
+# 	answer = Viterbi(hmm,test_py,5)[0].path
+# except:
+# 	answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
+# print(answer)
 
 # print(Viterbi(hmm,['baobao','ye','tai','bang','le','ba'],5))
