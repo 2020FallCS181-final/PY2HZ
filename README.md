7c1213ccf81437a38161e5efe31d501420d32d07
WuDaqian <wudq1@shanghaitech.edu.cn>
2021-01-08 21:00:08 +0800

    README

--- README.md
+++ README.md
@@ -1,11 +1,45 @@
 # AI Final - PY2HZ
 
+## Part3  Translation
+
+### How to test:
+
+In the file `test.py`, change `PATH` to the path of corresponding test set you want to test on. Then run ` test.py`.
+
+The test set should be a list of dictionary where each dictionary contains at least `"py","hz"` which corresponds to the input and ground truth output.
+
+```python
+[
+    {
+        "hz": [
+            "\u4e0d",
+            "\u5c11",
+            "\u654f\u611f",
+            "\u7684",
+            "\u8bfb\u8005",
+            "\u731c\u6d4b",
+            "\u8bf4"
+        ],
+        "py": [
+            "bu",
+            "shao",
+            "mingan",
+            "de",
+            "duzhe",
+            "caice",
+            "shuo"
+        ]
+    }
+]
+```
+
+
+
+
+
+
+
 ## Record Document
 
 > [Tencent Document](https://docs.qq.com/doc/DTEhqUHR1ck1Tak9s)
 
-### Links
-
-> [BaiDu/DDParser](https://github.com/baidu/DDParser)
-
-> [THUOCL](http://thuocl.thunlp.org)
--- part3/test.py
+++ part3/test.py
@@ -7,6 +7,9 @@ from tqdm import tqdm
 
 from seg_with_trie_consider_next import Trie_Tree
 tree = Trie_Tree('root')
+hmm = HmmParam()
+
+PATH = './test3/test_set_mixed.json'
 
 def norm_pinyin(pylist):
 	result = []
@@ -19,27 +22,17 @@ def norm_pinyin(pylist):
 	    	result.append(pinyin)
 	return result
 
-with open('./test3/test_set_mixed.json') as f:
-	test_set_mixed = json.load(f)
-
-# with open('./test3/test_set_9plus.json') as f:
-# 	test_set_9plus = json.load(f)
-
-# with open('./test3/test_set_3to5.json') as f:
-# 	test_set_3to5 = json.load(f)
-
-# with open('./test3/test_set_6to8.json') as f:
-# 	test_set_6to8 = json.load(f)
-hmm = HmmParam()
+with open(PATH) as f:
+	test_set = json.load(f)
 
 count_single = 0
 correct_single = 0
 count_sentence = 0
 correct_sentence = 0
 
-for i in tqdm(range(len(test_set_mixed))):
+for i in tqdm(range(len(test_set))):
 	count_sentence += 1
-	test = test_set_mixed[i]
+	test = test_set[i]
 	flag = True
 	test_py = tree.search(''.join(test['py'])).split(' ')
 	# print(test_py)
@@ -62,94 +55,3 @@ for i in tqdm(range(len(test_set_mixed))):
 print('set')
 print('single:',correct_single/count_single)
 print('sentence:',correct_sentence/count_sentence)
-
-# count_single = 0
-# correct_single = 0
-# count_sentence = 0
-# correct_sentence = 0
-
-# for i in tqdm(range(len(test_set_3to5))):
-# 	count_sentence += 1
-# 	test = test_set_3to5[i]
-# 	flag = True
-# 	test_py = tree.search(''.join(test['py'])).split(' ')
-# 	# print(test_py)
-# 	try:
-# 		answer = Viterbi(hmm,test_py,5)[0].path
-# 	except:
-# 		try:
-# 			answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
-# 		except:
-# 			continue
-# 	ground_truth = [c for c in ''.join(test['hz'])]
-# 	for idx,an in enumerate(answer):
-# 		count_single += 1
-# 		if an == ground_truth[idx]:
-# 			correct_single += 1
-# 		else:
-# 			flag = False
-# 	if flag:
-# 		correct_sentence += 1
-# print('set')
-# print('single:',correct_single/count_single)
-# print('sentence:',correct_sentence/count_sentence)
-
-# count_single = 0
-# correct_single = 0
-# count_sentence = 0
-# correct_sentence = 0
-# for i in tqdm(range(len(test_set_6to8))):
-# 	count_sentence += 1
-# 	test = test_set_6to8[i]
-# 	flag = True
-# 	test_py = tree.search(''.join(test['py'])).split(' ')
-# 	# print(test_py)
-# 	try:
-# 		answer = Viterbi(hmm,test_py,5)[0].path
-# 	except:
-# 		try:
-# 			answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
-# 		except:
-# 			continue
-# 	ground_truth = [c for c in ''.join(test['hz'])]
-# 	for idx,an in enumerate(answer):
-# 		count_single += 1
-# 		if an == ground_truth[idx]:
-# 			correct_single += 1
-# 		else:
-# 			flag = False
-# 	if flag:
-# 		correct_sentence += 1
-# print('6-8')
-# print('single:',correct_single/count_single)
-# print('sentence:',correct_sentence/count_sentence)
-
-# count_single = 0
-# correct_single = 0
-# count_sentence = 0
-# correct_sentence = 0
-# for i in tqdm(range(len(test_set_9plus))):
-# 	count_sentence += 1
-# 	test = test_set_9plus[i]
-# 	flag = True
-# 	test_py = tree.search(''.join(test['py'])).split(' ')
-# 	# print(test_py)
-# 	try:
-# 		answer = Viterbi(hmm,test_py,5)[0].path
-# 	except:
-# 		try:
-# 			answer = Viterbi(hmm,norm_pinyin(test_py),5)[0].path
-# 		except:
-# 			continue
-# 	ground_truth = [c for c in ''.join(test['hz'])]
-# 	for idx,an in enumerate(answer):
-# 		count_single += 1
-# 		if an == ground_truth[idx]:
-# 			correct_single += 1
-# 		else:
-# 			flag = False
-# 	if flag:
-# 		correct_sentence += 1
-# print('9+')
-# print('single:',correct_single/count_single)
-# print('sentence:',correct_sentence/count_sentence)
